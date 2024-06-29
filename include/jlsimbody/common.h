60ac3d4e99566ec025fedca2daeda5ade4a1eb59
Allen Hill <allenofthehills@gmail.com>
2024-06-28 17:09:01 -0700

    Fix __HERE__ definition

--- include/jlsimbody/common.h
+++ include/jlsimbody/common.h
@@ -13,17 +13,17 @@
 #ifdef VERBOSE_IMPORT
     namespace jlsimbody {
         static int r = 0;
-        const char* wrapping = "Wrapping";
+        static const char* wrapping = "Wrapping";
     }
 
 #  define __HERE__  __FILE__ ":" QUOTE2(__LINE__)
 #  define DEBUG_MSG(a) r = std::fprintf(stderr, "%s %s", wrapping, a); std::fflush(stderr)
 #  define CLEAR_DEBUG_MSG() std::cerr << std::string(r,'\x08') << "\x1B[J"
 #else
+#  define __HERE__
 #  define DEBUG_MSG(a)
 #  define CLEAR_DEBUG_MSG()
 #endif
-#define __HERE__
 #define QUOTE(arg) #arg
 #define QUOTE2(arg) QUOTE(arg)
 
